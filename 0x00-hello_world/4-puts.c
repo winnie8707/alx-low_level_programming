4451ffa321359032932d7da1d82e7090be528eda
winnie8707 <123winnie.namirimu@gmail.com>
2022-06-29 02:25:36 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,6 @@
-nclude <stdio.h>
+#include <stdio.h>
+
+
 
 /**
  *
@@ -9,8 +11,13 @@ nclude <stdio.h>
  *    * Return: Always 0 (Success)
  *
  *     */
+
 int main(void)
+
 {
+
 		puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+
+			return (0);
+
 }
