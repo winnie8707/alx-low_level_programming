733862ddebc2a4a149735862d21b206348255243
winnie8707 <123winnie.namirimu@gmail.com>
2022-07-01 16:16:02 +0300

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- *main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
+ *main - Entry point, print 00 to 99 using putchar
  *Return: Always 0 (Success)
  */
 
@@ -9,31 +9,17 @@ int main(void)
 {
 	int tens;
 	int ones;
-	int t;
-	int o;
 
-	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
+	for (tens = '0'; tens <= '9'; tens++) /*print tens place*/
 	{
-		for (ones = '0'; ones <= '9'; ones++)
+		for (ones = '0'; ones <= '9'; ones++) /*print ones place*/
 		{
-		 	for (t = tens; t <= '9'; t++) /*print second of pair*/
+			putchar(tens);
+			putchar(ones);
+			if (!(tens == '9' && ones == '9')) /*skip comma at end*/
 			{
-				for (o = ones + 1; o <= '9'; o++)
-				{
-					putchar(tens);
-					putchar(ones);
-					putchar(' ');
-					putchar(t);
-					putchar(o);
-
-					if (!((tens == '9' && ones == '8') &&
-					      (t == '9' && o == '9')))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				o = '0';
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
