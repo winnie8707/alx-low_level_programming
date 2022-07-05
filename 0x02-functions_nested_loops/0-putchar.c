eceacbed6f794e534468671c59723ccc352f3728
winnie8707 <123winnie.namirimu@gmail.com>
2022-07-05 12:05:30 +0300

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * main - print _putchar, followed by new line
+ * main - print Holberton, followed by new line
  * Return: 0
  */
 
 int main(void)
 {
-	_putchar('-');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
 	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 
 	return (0);
